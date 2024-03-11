a18efb017f9891b04e728b2d423a1127bf11d337
Felipe0408 <felipe.castillo@utp.ac.pa>
2024-03-11 15:54:32 +0000

    Se movió el side bar side A del Login.htm al componente pertinente

--- src/app/side-bar-side-a/side-bar-side-a.component.ts
+++ src/app/side-bar-side-a/side-bar-side-a.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-side-bar-side-a',
   templateUrl: './side-bar-side-a.component.html',
   styleUrls: ['./side-bar-side-a.component.scss']
 })
-export class SideBarSideAComponent {
+export class SideBarSideAComponent implements OnInit{
+  @Input() ShowRegisterTextchild:any;
 
+  constructor() {}
+  ngOnInit() {}
 }
